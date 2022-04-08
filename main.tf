ea771c692002f50a0b7676ec04edb014189243f3
Andy James <andy@andyjames.org>
2022-04-08 03:35:20 -0500

    some cloud init stuff

--- main.tf
+++ main.tf
@@ -15,11 +15,11 @@ data "template_file" "user_data" {
 }
 
 resource "aws_instance" "injection_demo" {
-  ami           = "ami-09fa80f679072f684"
-  instance_type = "t3.small"
-  key_name = "${var.ssh_key_name}"
+  ami             = "ami-09fa80f679072f684"
+  instance_type   = "t3.small"
+  key_name        = "${var.ssh_key_name}"
   security_groups = [aws_security_group.injection_demo.name]
-  user_data                   = data.template_file.user_data.rendered
+  user_data       = data.template_file.user_data.rendered
   tags = {
     Name = "HelloWorld"
   }
