5f05e2cdb49c2617afce50fec339ca7017555f37
Andy James <andy@andyjames.org>
2022-04-08 03:18:13 -0500

    cloud init test

--- main.tf
+++ main.tf
@@ -10,11 +10,16 @@ terraform {
 provider "aws" {
 }
 
+data "template_file" "user_data" {
+  template = file("scripts/cloud-init.yaml")
+}
+
 resource "aws_instance" "injection_demo" {
   ami           = "ami-09fa80f679072f684"
   instance_type = "t3.small"
   key_name = "${var.ssh_key_name}"
   security_groups = [aws_security_group.injection_demo.name]
+  user_data                   = data.template_file.user_data.rendered
   tags = {
     Name = "HelloWorld"
   }
