e445a47148217bb48b26181c89d7c28525f5811c
Andy James <andy@andyjames.org>
2022-04-08 04:30:31 -0500

    removing ansible

--- variables.tf
+++ variables.tf
@@ -1,12 +1,2 @@
-variable "ssh_user_name" {
-    default = "bitnami"
-}
 variable "ssh_key_name" {
-    default = "andy"
-}
-variable "ssh_key_path" {
-    default = "~/.ssh/aws_key"
-}
-variable "dev_host_label" {
-    default = "terra_ansible_host"
 }
